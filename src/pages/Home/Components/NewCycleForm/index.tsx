110fce5f6eb9dab19fa80f55ce591b1b91090602
Lucca Ambrosius <ismartlucca@gmail.com>
2023-01-08 19:52:50 -0300

    Context on Form

--- src/pages/Home/Components/NewCycleForm/index.tsx
+++ src/pages/Home/Components/NewCycleForm/index.tsx
@@ -1,6 +1,12 @@
 import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
+import { useContext } from 'react'
+import { CyclesContext } from '../..'
+import { useFormContext } from 'react-hook-form'
 
 export function NewCycleForm() {
+  const { activeCycle } = useContext(CyclesContext)
+  const { register } = useFormContext()
+
   return (
     <FormContainer>
       <label htmlFor="task">Vou trabalhar em</label>
@@ -11,14 +17,12 @@ export function NewCycleForm() {
         disabled={!!activeCycle}
         {...register('task')}
       />
-
       <datalist id="task-suggestions">
         <option value="Projeto 1" />
         <option value="Projeto 2" />
         <option value="Projeto 3" />
         <option value="Banana" />
       </datalist>
-
       <label htmlFor="minutesAmount">durante</label>
       <MinutesAmountInput
         type="number"
@@ -30,7 +34,6 @@ export function NewCycleForm() {
         disabled={!!activeCycle}
         {...register('minutesAmount', { valueAsNumber: true })}
       />
-
       <span>minutos.</span>
     </FormContainer>
   )
